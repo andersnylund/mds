f4487b7169c97fe33ca11197b6a4866dbea3ab24
Anders Nylund <anders.nylund.an@gmail.com>
2018-03-30 13:54:24 +0300

    Completely solve problem 4.1

--- assignment3/problem4-1.py
+++ assignment3/problem4-1.py
@@ -6,11 +6,14 @@ spark = pyspark.SparkContext(master="local[*]", appName="movie")
 
 lines = spark.textFile("./data/movielens/ratings.csv")
 
-result = lines.map(lambda line: line.split(",")) \ 
+# remove header, create a tuple of each row, groupByKey and mapValues reduces by key and sums the count of each movie)
+interm = lines.map(lambda line: line.split(",")) \
     .filter(lambda line: line[0] != "user_id") \
     .map(lambda line: (int(line[1]), 1)) \
     .groupByKey() \
     .mapValues(len) \
     .collect()
+interm.sort(key=lambda tup: tup[0]) # sort the list by movie_id
 
-result.sort(key=lambda tup: tup[0])
+max_ratings = max(interm, key=lambda item: item[1])[1] # get movie with most ratings
+result = list(filter(lambda movie: movie[1]/max_ratings >= 0.25, interm)) # filter list with 25th percentile
